cfa776981e0d2dee63f347c207dd0e0813eaca8b
Shahin4292 <sarker4292@gmail.com>
2025-03-12 15:36:09 +0600

    color path added

---
+++ lib/utils/color_path.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class ColorPath{
+  static const Color grey = Color(0xfff1976cc);
+  static const Color greyShade = Color(0xfffcbe5ff);
+  static const Color red = Color(0xfff607e99);
+  static const Color redShade = Color(0xfff4e708f);
+}
