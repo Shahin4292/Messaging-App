71dbcf5696245a048b76f2d21e24b2a8dd483395
Shahin4292 <sarker4292@gmail.com>
2025-03-12 15:29:25 +0600

    updated

---
+++ lib/view/chat_details/chat_details.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatDetails extends StatelessWidget {
+  const ChatDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
