834f90e2640b42716598837742b9372de0112071
Shahin4292 <sarker4292@gmail.com>
2025-03-12 13:59:16 +0600

    add inbox list

---
+++ lib/res/repository/inbox_list.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+List<Map<String, dynamic>> inboxes = [
+  {"name": "All Inboxes", "icon": Icons.all_inbox},
+  {"name": "Alpha Pbx (W)", "icon": Icons.public},
+  {"name": "Pbx.com.bd (Fb)", "icon": Icons.chat},
+  {"name": "Sms.net.bd (W)", "icon": Icons.sms},
+  {"name": "Ecard Livechat", "icon": Icons.support_agent},
+  {"name": "Go Digital Bd Livechat", "icon": Icons.headset_mic},
+  {"name": "Go Digital Bd (Fb)", "icon": Icons.facebook},
+  {"name": "Alpha Pbx Livechat", "icon": Icons.phone},
+  {"name": "Alpha.net.bd Livechat", "icon": Icons.web},
+  {"name": "Lawbd Website", "icon": Icons.gavel},
+  {"name": "Alpha Sms Livechat", "icon": Icons.message},
+  {"name": "Alpha Sms (Fb)", "icon": Icons.facebook},
+  {"name": "Ecard.com.bd (Fb)", "icon": Icons.facebook},
+  {"name": "Alpha Net - Online Shop (Fb)", "icon": Icons.shopping_cart},
+];
