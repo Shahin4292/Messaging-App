dd5e845034fe9459654373c50aa6f7ecf7493f43
Shahin4292 <sarker4292@gmail.com>
2025-03-13 10:49:17 +0600

    add almost done

---
+++ lib/view/settings/widget/selectable_status_title.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class SelectableStatusTile extends StatelessWidget {
+  final String label;
+  final int selectedTileIndex;
+  final int index;
+  final Color color;
+  final ValueChanged<int> onTap;
+
+  const SelectableStatusTile({
+    super.key,
+    required this.label,
+    required this.selectedTileIndex,
+    required this.index,
+    required this.onTap, required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onTap(index); // Trigger onTap with the current index
+        Navigator.pop(context); // Close the bottom sheet
+      },
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(color: Colors.transparent),
+        child: Row(
+          children: [
+            CircleAvatar(
+              backgroundColor: color,
+              radius: 6, // Avatar size
+            ),
+            SizedBox(width: 10), // Add space between CircleAvatar and text
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: Colors.black,
+              ),
+            ),
+            Spacer(),
+            if (selectedTileIndex == index)
+              Icon(Icons.check, size: 20, color: Colors.blue),
+          ],
+        ),
+      ),
+    );
+  }
+}
