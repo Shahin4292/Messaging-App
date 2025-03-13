51d4c7dd036d3a53626d19599ec4499f6ce5163d
Shahin4292 <sarker4292@gmail.com>
2025-03-13 12:04:33 +0600

    add all color path

--- lib/utils/color_path.dart
+++ lib/utils/color_path.dart
@@ -5,4 +5,6 @@ class ColorPath{
   static const Color greyShade = Color(0xfffcbe5ff);
   static const Color red = Color(0xfff607e99);
   static const Color redShade = Color(0xfff4e708f);
+  static const Color shade = Color(0xfffda375a);
+  static const Color green = Color(0xfff808080);
 }
