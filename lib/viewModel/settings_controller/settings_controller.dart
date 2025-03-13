f568354d1fa7e11ed855dc7cfb1cca2d440f3a5b
Shahin4292 <sarker4292@gmail.com>
2025-03-13 11:04:36 +0600

    add settings controller almost done

---
+++ lib/viewModel/settings_controller/settings_controller.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../res/repository/language_list.dart';
+import '../../view/settings/widget/selectable_status_title.dart';
+import '../../view/settings/widget/selectable_tile.dart';
+
+class SettingsController extends GetxController {
+  int selectedTileIndex = -1;
+  String selectedLanguage = "English";
+
+  void showLanguageBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.symmetric(vertical: 12),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ModifyText(title: "Set Language"),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: languages.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        ListTile(
+                          title: Text(
+                            languages[index],
+                            style: TextStyle(fontSize: 14),
+                          ),
+                          trailing:
+                              selectedLanguage == languages[index]
+                                  ? Icon(
+                                    Icons.check,
+                                    color: Colors.blue,
+                                    size: 18,
+                                  )
+                                  : null,
+                          onTap: () {
+                            selectedLanguage = languages[index];
+                            Navigator.pop(context); // Close BottomSheet
+                          },
+                        ),
+                        if (index < languages.length - 1)
+                          Padding(
+                            padding: const EdgeInsets.only(left: 18),
+                            child: Divider(),
+                          ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void sortBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return Container(
+          width: MediaQuery.sizeOf(context).width,
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ModifyText(title: 'Switch Account'),
+              SelectableTile(
+                name: 'Sineris',
+                role: 'Administrator',
+                selectedTileIndex: selectedTileIndex,
+                index: 0,
+                onTap: (index) {
+                  selectedTileIndex = index;
+                },
+              ),
+              CustomPadding(),
+              SelectableTile(
+                name: 'Alpha Net',
+                role: 'Administrator',
+                selectedTileIndex: selectedTileIndex,
+                index: 1,
+                onTap: (index) {
+                  selectedTileIndex = index;
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void showSetIconBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (context) {
+        return Container(
+          width: MediaQuery.sizeOf(context).width,
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ModifyText(title: 'Set yourself as'),
+              SelectableStatusTile(
+                label: 'Online',
+                selectedTileIndex: selectedTileIndex,
+                index: 0,
+                onTap: (index) {
+                  selectedTileIndex = index;
+                },
+                color: Colors.amberAccent,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 18),
+                child: Divider(height: 1, color: Colors.grey[300]),
+              ),
+              SelectableStatusTile(
+                label: 'Busy',
+                selectedTileIndex: selectedTileIndex,
+                index: 1,
+                onTap: (index) {
+                  selectedTileIndex = index;
+                },
+                color: Colors.grey,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 18),
+                child: Divider(height: 1, color: Colors.grey[300]),
+              ),
+              SelectableStatusTile(
+                label: 'Offline',
+                selectedTileIndex: selectedTileIndex,
+                index: 2,
+                onTap: (index) {
+                  selectedTileIndex = index;
+                },
+                color: Colors.red,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
