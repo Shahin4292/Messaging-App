c0e54831014dd6a5d842c99de54d116a923d29b6
Shahin4292 <sarker4292@gmail.com>
2025-03-13 10:52:04 +0600

    add list of language string

---
+++ lib/res/repository/language_list.dart
@@ -0,0 +1,13 @@
+List<String> languages = [
+  "Afrikaans",
+  "Arabic",
+  "Bahasa Indonesia",
+  "Catalan",
+  "Czech",
+  "Danish",
+  "German",
+  "English",
+  "Greek",
+  "Spanish",
+  "Farsi",
+];
