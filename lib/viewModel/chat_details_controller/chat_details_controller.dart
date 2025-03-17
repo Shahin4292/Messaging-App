3a83bdd7f58f5644db17f8ea960df5a22fbbdb18
Shahin4292 <sarker4292@gmail.com>
2025-03-17 15:37:28 +0600

    update

--- lib/viewModel/chat_details_controller/chat_details_controller.dart
+++ lib/viewModel/chat_details_controller/chat_details_controller.dart
@@ -1,83 +1,44 @@
-
-import 'dart:io' show File;
-
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
-class ChatDetailsController extends GetxController{
+class ChatDetailsController extends GetxController {
+  var messages = <Map<String, dynamic>>[].obs;
+  var selectedImages = <File>[].obs;
   TextEditingController controller = TextEditingController();
-  List<Map<String, dynamic>> messages = [];
-  List<File> selectedImages = [];
-  ScrollController scrollController = ScrollController();
 
   static const int maxImages = 10;
 
   Future<void> pickImage(ImageSource source) async {
     final pickedFile = await ImagePicker().pickImage(source: source);
     if (pickedFile != null && selectedImages.length < maxImages) {
-        selectedImages.add(File(pickedFile.path));
+      selectedImages.add(File(pickedFile.path));
     } else if (selectedImages.length >= maxImages) {
-      Get.snackbar('Image Limitation', 'You can only select up to $maxImages images');
+      Get.snackbar(
+        'Limit reached',
+        'You can only select up to $maxImages images',
+        snackPosition: SnackPosition.BOTTOM,
+      );
     }
   }
 
   void sendMessage() {
     if (selectedImages.isNotEmpty || controller.text.isNotEmpty) {
-        if (selectedImages.isNotEmpty) {
-          for (var image in selectedImages) {
-            messages.add({"type": "image", "content": image.path});
-          }
-          selectedImages.clear();
+      if (selectedImages.isNotEmpty) {
+        for (var image in selectedImages) {
+          messages.insert(0, {"type": "image", "content": image.path});
         }
-        if (controller.text.isNotEmpty) {
-          messages.add({"type": "text", "content": controller.text});
-        }
-        controller.clear();
-
-      scrollToBottom();
+        selectedImages.clear();
+      }
+      if (controller.text.trim().isNotEmpty) {
+        messages.insert(0, {"type": "text", "content": controller.text});
+      }
+      controller.clear();
     }
   }
 
-  void scrollToBottom() {
-    if (scrollController.hasClients) {
-      scrollController.animateTo(
-        scrollController.position.maxScrollExtent * 2,
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeOut,
-      );
-    }
-  }
-
-  void showImageOptions(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (context) {
-        return Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              ListTile(
-                leading: Icon(Icons.camera_alt),
-                title: Text("Take a Photo"),
-                onTap: () {
-                  Navigator.pop(context);
-                  pickImage(ImageSource.camera);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.image),
-                title: Text("Pick from Gallery"),
-                onTap: () {
-                  Navigator.pop(context);
-                  pickImage(ImageSource.gallery);
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
+  void clearSelectedImages() {
+    selectedImages.clear();
   }
 }
