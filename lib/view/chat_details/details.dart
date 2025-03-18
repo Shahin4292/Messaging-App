a560ec69aefa658c059e9870494a17f1410d6563
Shahin4292 <sarker4292@gmail.com>
2025-03-18 14:20:30 +0600

    add

---
+++ lib/view/chat_details/details.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DetailsView extends StatelessWidget {
+  const DetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      key: const ValueKey(2),
+      color: Colors.red,
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+
+        ],
+      )
+    );
+  }
+}
