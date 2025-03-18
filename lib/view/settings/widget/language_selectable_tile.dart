124e26ab7f6b07050ef280d65aa98374786c8eb6
Shahin4292 <sarker4292@gmail.com>
2025-03-18 11:53:08 +0600

    remove listTile

--- lib/view/settings/widget/language_selectable_tile.dart
+++ lib/view/settings/widget/language_selectable_tile.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-class InboxTile extends StatelessWidget {
+class LanguageSelectableTile extends StatelessWidget {
   final Map<String, dynamic> inbox;
   final bool isSelected;
   final VoidCallback onTap;
 
-  const InboxTile({super.key,
+  const LanguageSelectableTile({
+    super.key,
     required this.inbox,
     required this.isSelected,
     required this.onTap,
@@ -16,26 +17,19 @@ class InboxTile extends StatelessWidget {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: EdgeInsets.only(left: 14,right: 14,top: 6,bottom: 6),
+        padding: EdgeInsets.only(left: 14, right: 14, top: 4, bottom: 4),
         decoration: BoxDecoration(
           color: Colors.transparent,
           borderRadius: BorderRadius.circular(8),
         ),
         child: Row(
           children: [
-            Icon(
-              inbox["icon"],
-              size: 18,
-              color: Colors.grey,
-            ),
-            SizedBox(width: 10),
             Text(
-              inbox["name"], // Display the name
-              style: TextStyle(fontSize: 14,fontFamily: 'Inter'),
+              inbox["name"],
+              style: TextStyle(fontSize: 14, fontFamily: 'Inter'),
             ),
             Spacer(),
-            if (isSelected)
-              Icon(Icons.check, color: Colors.blue, size: 18),
+            if (isSelected) Icon(Icons.check, color: Colors.blue, size: 18),
           ],
         ),
       ),
