21b3b5621abfed43a7112e090d6cde134370f7be
Shahin4292 <sarker4292@gmail.com>
2025-03-19 12:39:00 +0600

    splash almost done

--- lib/view/splash_view/splash_view.dart
+++ lib/view/splash_view/splash_view.dart
@@ -1,32 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'dart:async';
 
 import '../../res/components/bottom_bar.dart';
+
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
-  State<SplashView> createState() => _SplashViewState();
+  SplashViewState createState() => SplashViewState();
 }
 
-class _SplashViewState extends State<SplashView> {
-
+class SplashViewState extends State<SplashView> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => CustomBottomNavScreen()),
-      );
+
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.pushReplacement(context, createRoute());
     });
   }
 
+  Route createRoute() {
+    return PageRouteBuilder(
+      pageBuilder:
+          (context, animation, secondaryAnimation) => CustomBottomNavScreen(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(-1.0, 0.0);
+        const end = Offset.zero;
+        const curve = Curves.easeInOut;
+
+        var tween = Tween(
+          begin: begin,
+          end: end,
+        ).chain(CurveTween(curve: curve));
+        var offsetAnimation = animation.drive(tween);
+
+        var fadeAnimation = Tween(begin: 0.0, end: 1.0).animate(animation);
+
+        return SlideTransition(
+          position: offsetAnimation,
+          child: FadeTransition(opacity: fadeAnimation, child: child),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueAccent,
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Lottie.asset(
+          'assets/fonts/splash_animation.json',
+          width: 350, // Animation size
+          height: 350,
+          fit: BoxFit.fill,
+        ),
       ),
     );
   }
