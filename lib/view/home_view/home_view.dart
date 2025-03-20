72bd599ebaa6dafefa134e0a83547318461c09fd
Shahin4292 <sarker4292@gmail.com>
2025-03-20 11:47:01 +0600

    add post input

--- lib/view/home_view/home_view.dart
+++ lib/view/home_view/home_view.dart
@@ -13,48 +13,47 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        title: Row(
-          children: [
-            Text(
-              "Message",
-              style: TextStyle(
-                color: Colors.blue,
-                fontWeight: FontWeight.bold,
-                fontSize: 24,
-              ),
+        actions: [
+          SizedBox(width: 10),
+          Text(
+            "Message",
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
             ),
-            Spacer(),
-            GestureDetector(
-              onTap: () {
-                // Navigator.pushReplacement(
-                //   context,
-                //   MaterialPageRoute(builder: (context) => CustomBottomNavScreen()),
-                // );
-              },
-              child: Container(
-                height: 25,
-                width: 25,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/icons/message.png"),
-                  ),
+          ),
+          Spacer(),
+          GestureDetector(
+            onTap: () {
+              // Navigator.pushReplacement(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => CustomBottomNavScreen()),
+              // );
+            },
+            child: Container(
+              height: 25,
+              width: 25,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/icons/message.png"),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          SizedBox(width: 10),
+        ],
       ),
       body: Column(
         children: [
           StorySection(),
-          // PostComposer(),
+          PostComposer(),
           // Expanded(child: PostFeed()),
         ],
       ),
     );
   }
 }
-
 
 class StorySection extends StatelessWidget {
   StorySection({super.key});
@@ -64,13 +63,12 @@ class StorySection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 80,
+      height: 70,
       color: Colors.white,
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 2),
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         shrinkWrap: true,
-        // physics: NeverScrollableScrollPhysics(),
         itemCount: controller.messages.length,
         itemBuilder: (context, index) {
           final message = controller.messages[index];
@@ -79,35 +77,36 @@ class StorySection extends StatelessWidget {
             child: Stack(
               children: [
                 CircleAvatar(
-                  radius: 35,
+                  radius: 30,
                   backgroundColor: ColorPath.greyShade,
                   backgroundImage:
-                  message.imageUrl != null
-                      ? AssetImage(message.imageUrl!)
-                      : null,
+                      message.imageUrl != null
+                          ? AssetImage(message.imageUrl!)
+                          : null,
                   child:
-                  message.imageUrl == null
-                      ? (message.icon != null
-                      ? Icon(message.icon, color: ColorPath.grey)
-                      : Text(
-                    message.sender
-                        .split(" ")
-                        .map((e) => e[0])
-                        .take(2)
-                        .join()
-                        .toUpperCase(),
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: ColorPath.grey,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ))
-                      : null,
+                      message.imageUrl == null
+                          ? (message.icon != null
+                              ? Icon(message.icon, color: ColorPath.grey)
+                              : Text(
+                                message.sender
+                                    .split(" ")
+                                    .map((e) => e[0])
+                                    .take(2)
+                                    .join()
+                                    .toUpperCase(),
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: ColorPath.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ))
+                          : null,
                 ),
                 Positioned(
-                    top: 2,right: 2,
-                    child: CircleAvatar(
-                      radius: 7, backgroundColor: Colors.green))
+                  top: 2,
+                  right: 2,
+                  child: CircleAvatar(radius: 7, backgroundColor: Colors.green),
+                ),
               ],
             ),
           );
@@ -117,30 +116,37 @@ class StorySection extends StatelessWidget {
   }
 }
 
-// class PostComposer extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-//       child: Row(
-//         children: [
-//           CircleAvatar(radius: 20, backgroundColor: Colors.grey.shade300),
-//           SizedBox(width: 10),
-//           Expanded(
-//             child: Container(
-//               padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-//               decoration: BoxDecoration(
-//                 borderRadius: BorderRadius.circular(20),
-//                 border: Border.all(color: Colors.grey.shade300),
-//               ),
-//               child: Text("What's on your mind?"),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+class PostComposer extends StatelessWidget {
+  const PostComposer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 20,
+            backgroundColor: Colors.grey.shade300,
+            child: Center(child: Image.asset("assets/icons/user.png",height: 25,width: 25)),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.grey.shade300),
+              ),
+              child: Text("What's on your mind?"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 //
 // class PostFeed extends StatelessWidget {
 //   @override
