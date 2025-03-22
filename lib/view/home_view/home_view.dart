5d8be0e9562f7ee7d8082e2401338dc30000f132
Shahin4292 <sarker4292@gmail.com>
2025-03-22 12:32:03 +0600

    add widget

--- lib/view/home_view/home_view.dart
+++ lib/view/home_view/home_view.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:messaging_app/view/home_view/widget/post_composer.dart';
+import 'package:messaging_app/view/home_view/widget/post_feed.dart';
 import '../../utils/color_path.dart';
 import '../../viewModel/chat_controller/chat_controller.dart';
-import '../../viewModel/home_controller/home_controller.dart';
-import '../chat_details/widget/date_time.dart';
 import '../slider_view/slider_view.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+  HomeView({super.key});
+
+  final ChatController controller = Get.put(ChatController());
 
   @override
   Widget build(BuildContext context) {
@@ -31,217 +33,74 @@ class HomeView extends StatelessWidget {
           ),
           Spacer(),
           GestureDetector(
-            onTap: () {
-              // Navigator.pushReplacement(
-              //   context,
-              //   MaterialPageRoute(builder: (context) => CustomBottomNavScreen()),
-              // );
-            },
-            child: Icon(CupertinoIcons.chat_bubble_text,size: 27,color: Colors.blue),
-          ),
-          SizedBox(width: 10),
-        ],
-      ),
+            onTap: () {},
+            child: Icon(
+              CupertinoIcons.chat_bubble_text,
+              size: 27,
+              color: Colors.blue)),
+          SizedBox(width: 10)]),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            StorySection(),
+            Container(
+              height: 70,
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 2),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: controller.messages.length,
+                itemBuilder: (context, index) {
+                  final message = controller.messages[index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder:
+                              (context) => SlideView(
+                                sender: message.sender,
+                                imageUrl: message.imageUrl,
+                                message: message.message,
+                                isDeleted: message.isDeleted,
+                                additionalIcon: message.additionalIcon,
+                                additionalText: message.additionalText,
+                                timestamp: message.timestamp,
+                                icon: message.icon)));},
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      child: Stack(
+                        children: [
+                          CircleAvatar(
+                            radius: 30,
+                            backgroundColor: ColorPath.greyShade,
+                            backgroundImage:
+                                message.imageUrl != null
+                                    ? AssetImage(message.imageUrl!)
+                                    : null,
+                            child:
+                                message.imageUrl == null
+                                    ? (message.icon != null
+                                        ? Icon(
+                                          message.icon,
+                                          color: ColorPath.grey)
+                                        : Text(
+                                          message.sender
+                                              .split(" ")
+                                              .map((e) => e[0])
+                                              .take(2)
+                                              .join()
+                                              .toUpperCase(),
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            color: ColorPath.grey,
+                                            fontWeight: FontWeight.bold)))
+                                    : null),
+                          Positioned(
+                            top: 2,
+                            right: 2,
+                            child: CircleAvatar(
+                              radius: 7,
+                              backgroundColor: Colors.green))])));})),
             PostComposer(),
-            PostFeed(),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class StorySection extends StatelessWidget {
-  StorySection({super.key});
-
-  final ChatController controller = Get.put(ChatController());
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 70,
-      color: Colors.white,
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 2),
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        itemCount: controller.messages.length,
-        itemBuilder: (context, index) {
-          final message = controller.messages[index];
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder:
-                      (context) => SlideView(
-                    sender: message.sender,
-                    imageUrl: message.imageUrl,
-                    message: message.message,
-                    isDeleted: message.isDeleted,
-                    additionalIcon: message.additionalIcon,
-                    additionalText: message.additionalText,
-                    timestamp: message.timestamp,
-                    icon: message.icon,
-                  ),
-                ),
-              );
-            },
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 5),
-              child: Stack(
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundColor: ColorPath.greyShade,
-                    backgroundImage:
-                        message.imageUrl != null
-                            ? AssetImage(message.imageUrl!)
-                            : null,
-                    child:
-                        message.imageUrl == null
-                            ? (message.icon != null
-                                ? Icon(message.icon, color: ColorPath.grey)
-                                : Text(
-                                  message.sender
-                                      .split(" ")
-                                      .map((e) => e[0])
-                                      .take(2)
-                                      .join()
-                                      .toUpperCase(),
-                                  style: TextStyle(
-                                    fontSize: 20,
-                                    color: ColorPath.grey,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ))
-                            : null,
-                  ),
-                  Positioned(
-                    top: 2,
-                    right: 2,
-                    child: CircleAvatar(radius: 7, backgroundColor: Colors.green),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class PostComposer extends StatelessWidget {
-  const PostComposer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        children: [
-          CircleAvatar(
-            radius: 20,
-            backgroundColor: Colors.grey.shade300,
-            child: Icon(Icons.person,size: 25,color: Colors.grey.shade500,)
-          ),
-          SizedBox(width: 10),
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(color: Colors.grey.shade300),
-              ),
-              child: Text("What's on your mind?"),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-
-class PostFeed extends StatelessWidget {
-   PostFeed({super.key});
-
-  final ChatController controller = Get.put(ChatController());
-  final HomeController homeController = Get.put(HomeController());
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      itemCount: controller.messages.length,
-      itemBuilder: (context, index) {
-        final message = controller.messages[index];
-        return Card(
-          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-          color: Colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ListTile(
-                leading: CircleAvatar(
-                  radius: 20,
-                  backgroundColor: ColorPath.greyShade,
-                  backgroundImage:
-                  message.imageUrl != null
-                      ? AssetImage(message.imageUrl!)
-                      : null,
-                  child:
-                  message.imageUrl == null
-                      ? (message.icon != null
-                      ? Icon(message.icon, color: ColorPath.grey)
-                      : Text(
-                    message.sender
-                        .split(" ")
-                        .map((e) => e[0])
-                        .take(2)
-                        .join()
-                        .toUpperCase(),
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: ColorPath.grey,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ))
-                      : null,
-                ),
-                title: Text(message.sender),
-                subtitle: Text(homeController.formattedTime),
-                trailing: Icon(Icons.more_horiz),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Text("This is a sample post description."),
-              ),
-              Container(
-                height: 200,
-                color: Colors.grey.shade300,
-                child: Center(child: Icon(Icons.image, size: 50, color: Colors.grey)),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    IconButton(icon: Icon(Icons.thumb_up_alt_outlined), onPressed: () {}),
-                    IconButton(icon: Icon(Icons.comment_outlined), onPressed: () {}),
-                    IconButton(icon: Icon(Icons.share_outlined), onPressed: () {}),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
+            PostFeed()])));}}
