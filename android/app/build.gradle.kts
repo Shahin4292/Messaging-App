3b13794cbfec67c680f318b4f9c2a3000b124271
Shahin4292 <sarker4292@gmail.com>
2025-03-30 10:19:06 +0600

    change package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.messaging_app"
+    namespace = "com.alphanet.messaging_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 //    ndkVersion = flutter.ndkVersion
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.messaging_app"
+        applicationId = "com.alphanet.messaging_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
