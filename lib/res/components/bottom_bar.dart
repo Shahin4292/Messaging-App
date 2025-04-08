ade69ae34edd3cffe056e8d8cafb5493202be961
Shahin4292 <sarker4292@gmail.com>
2025-04-08 11:24:41 +0600

    splash effect added

--- lib/res/components/bottom_bar.dart
+++ lib/res/components/bottom_bar.dart
@@ -35,49 +35,79 @@ class CustomBottomNavScreenState extends State<CustomBottomNavScreen> {
       backgroundColor: Colors.white,
       body: _pages[_selectedIndex],
       bottomNavigationBar: Container(
-        padding: const EdgeInsets.only(
-          bottom: 25,
-          left: 40,
-          right: 40,
-          top: 10,
-        ),
         decoration: BoxDecoration(
           color: Colors.white,
           border: Border.all(width: 1, color: Colors.grey.shade300),
         ),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            GestureDetector(
-              onTap: () => _onItemTapped(0),
-              child: Icon(
-                _selectedIndex == 0
-                    ? Icons.inbox_rounded
-                    : Icons.inbox_outlined,
-                color: _selectedIndex == 0 ? Colors.black : Colors.grey,
-                size: 30,
+            Material(
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius:  BorderRadius.circular(30),
+                onTap: () => _onItemTapped(0),
+                child: Container(
+                  padding: const EdgeInsets.only(
+                    bottom: 20,
+                    left: 30,
+                    right: 30,
+                    top: 10,
+                  ),
+                  child: Icon(
+                    _selectedIndex == 0
+                        ? Icons.inbox_rounded
+                        : Icons.inbox_outlined,
+                    color: _selectedIndex == 0 ? Colors.black : Colors.grey,
+                    size: 28,
+                  ),
+                ),
               ),
             ),
             // Chat Icon
-            GestureDetector(
-              onTap: () => _onItemTapped(1),
-              child: Icon(
-                _selectedIndex == 1
-                    ? CupertinoIcons.chat_bubble_fill
-                    : CupertinoIcons.chat_bubble,
-                color: _selectedIndex == 1 ? Colors.black : Colors.grey,
-                size: 30,
+            Material(
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius:  BorderRadius.circular(30),
+                onTap: () => _onItemTapped(1),
+                child: Container(
+                  padding: const EdgeInsets.only(
+                    bottom: 20,
+                    left: 30,
+                    right: 30,
+                    top: 10,
+                  ),
+                  child: Icon(
+                    _selectedIndex == 1
+                        ? CupertinoIcons.chat_bubble_fill
+                        : CupertinoIcons.chat_bubble,
+                    color: _selectedIndex == 1 ? Colors.black : Colors.grey,
+                    size: 28,
+                  ),
+                ),
               ),
             ),
             // Settings Icon
-            GestureDetector(
-              onTap: () => _onItemTapped(2),
-              child: Icon(
-                _selectedIndex == 2
-                    ? CupertinoIcons.gear_alt_fill
-                    : CupertinoIcons.gear_alt,
-                color: _selectedIndex == 2 ? Colors.black : Colors.grey,
-                size: 30,
+            Material(
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius:  BorderRadius.circular(30),
+                onTap: () => _onItemTapped(2),
+                child: Container(
+                  padding: const EdgeInsets.only(
+                    bottom: 20,
+                    left: 30,
+                    right: 30,
+                    top: 10,
+                  ),
+                  child: Icon(
+                    _selectedIndex == 2
+                        ? CupertinoIcons.gear_alt_fill
+                        : CupertinoIcons.gear_alt,
+                    color: _selectedIndex == 2 ? Colors.black : Colors.grey,
+                    size: 28,
+                  ),
+                ),
               ),
             ),
           ],
